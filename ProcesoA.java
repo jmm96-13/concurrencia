f5d9aad386125acc6dc875c2cb24b66c7bc47d8d
jmm96-13 <jmm.cv.96@gmail.com>
2025-11-04 14:35:17 +0100

    Implement random word generation in ProcesoA
    Fix syntax errors in ProcesoA.java and generate random words.
    practicar concurrencia

---
+++ ProcesoA.java
@@ -0,0 +1,19 @@
+import java.io.PrintWriter;
+import java.util.Random;
+
+public class ProcesoA {
+  public static void main(String[] args) {
+    String alfabeto = "abcdefghijklmnñopqrstuvwxyz";
+    Random aleatorio = new Random();
+    PrintWriter salida = new PrintWriter(System.out, true);
+
+    for (int contador) = 0; contador < 15; contador ++ {
+         int longitud = 5 + aleatorio.nextInt(11);
+         StringBuilder palabra = new StringBuilder(longitud);
+
+         for (int posicion = 0; posicion < longitud; posisicion++) {
+             palabra.append(alfabeto.charAt(aleatorio))
+         }
+  }
+
+}
